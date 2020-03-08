91e0d4ae8dbe3dce85f8f70ca10e25ab9b9e9891
Johnson <zxmagichacker@gmail.com>
2020-03-08 14:35:44 +0800

    feat: router重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/login",
     name: "login",
     component: Login
   },
@@ -14,6 +14,10 @@ const routes = [
     path: "/mainPage",
     name: "mainPage",
     component: MainPage
+  },
+  {
+    path: "/",
+    redirect: "/login"
   }
 ];
 
