7d6e655d9b0fa047eb5886f149cddd7f27f1588e
Johnson <zxmagichacker@gmail.com>
2020-03-11 14:33:31 +0800

    feat: 构建教师管理和学生管理组件

--- src/views/student-manage/index.vue
+++ src/views/student-manage/index.vue
@@ -1,8 +1,60 @@
 <template>
-  <div>22</div>
+  <div class="student-page-wrap">
+    <div class="student-search-wrap">
+      <el-input v-model="studentName" placeholder="姓名"></el-input>
+      <el-input v-model="phoneNumber" placeholder="手机号"></el-input>
+      <el-date-picker
+        v-model="startTime"
+        type="datetime"
+        placeholder="开始时间"
+      ></el-date-picker>
+      <el-date-picker
+        v-model="endTime"
+        type="datetime"
+        placeholder="结束时间"
+      ></el-date-picker>
+      <el-button type="primary">搜索</el-button>
+    </div>
+    <el-table border>
+      <el-table-column label="姓名"></el-table-column>
+      <el-table-column label="手机号"></el-table-column>
+      <el-table-column label="已购课程"></el-table-column>
+      <el-table-column label="注册时间"></el-table-column>
+    </el-table>
+    <pagination-view></pagination-view>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+import PaginationView from "../../components/pagination-view/index.vue";
+@Component({
+  components: {
+    PaginationView
+  }
+})
+export default class StudentManage extends Vue {
+  studentName = "";
+  phoneNumber = "";
+  startTime = "";
+  endTime = "";
+}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.student-page-wrap {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 30px 20px 0px;
+  .student-search-wrap {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    /deep/ .el-input {
+      width: 200px;
+    }
+  }
+}
+</style>
--- src/views/teacher-manage/index.vue
+++ src/views/teacher-manage/index.vue
@@ -1,8 +1,99 @@
 <template>
-  <div>22</div>
+  <div class="teacher-page-wrap">
+    <div class="teacher-search-wrap">
+      <el-input v-model="teacherName" placeholder="老师姓名"></el-input>
+      <el-input v-model="phoneNumber" placeholder="手机号"></el-input>
+      <el-select v-model="teacherStatus" placeholder="状态" clearable>
+        <el-option label="启用" value="0"></el-option>
+        <el-option label="停用" value="1"></el-option>
+      </el-select>
+      <el-date-picker
+        v-model="startTime"
+        type="datetime"
+        placeholder="开始时间"
+      ></el-date-picker>
+      <el-date-picker
+        v-model="endTime"
+        type="datetime"
+        placeholder="结束时间"
+      ></el-date-picker>
+      <el-button type="primary">搜索</el-button>
+    </div>
+    <div class="teacher-table-wrap">
+      <el-button type="primary" icon="el-icon-plus">新增</el-button>
+      <el-table border>
+        <el-table-column label="编号" align="center"></el-table-column>
+        <el-table-column label="头像" align="center"></el-table-column>
+        <el-table-column label="手机号" align="center"></el-table-column>
+        <el-table-column label="姓名" align="center"></el-table-column>
+        <el-table-column label="年级" align="center"></el-table-column>
+        <el-table-column label="科目" align="center"></el-table-column>
+        <el-table-column label="简介" align="center"></el-table-column>
+        <el-table-column label="佣金比例" align="center"></el-table-column>
+        <el-table-column label="收入" align="center"></el-table-column>
+        <el-table-column label="已体现" align="center"></el-table-column>
+        <el-table-column label="学生数" align="center"></el-table-column>
+        <el-table-column label="课程数" align="center"></el-table-column>
+        <el-table-column label="创建时间" align="center"></el-table-column>
+        <el-table-column label="状态" align="center"></el-table-column>
+        <el-table-column label="操作" align="center">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="editItem(scope.row)">编辑</el-button>
+            <el-button size="mini" @click="delItem(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <pagination-view></pagination-view>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+import PaginationView from "../../components/pagination-view/index.vue";
+@Component({
+  components: {
+    PaginationView
+  }
+})
+export default class TeacherManage extends Vue {
+  teacherName = "";
+  phoneNumber = "";
+  teacherStatus = "";
+  startTime = "";
+  endTime = "";
+  editItem(): void {
+    console.log("编辑");
+  }
+  delItem(): void {
+    console.log("删除");
+  }
+}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.teacher-page-wrap {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 30px 20px 0px;
+  .teacher-search-wrap {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    /deep/ .el-input {
+      width: 200px;
+    }
+  }
+  .teacher-table-wrap {
+    width: 100%;
+    margin-top: 20px;
+    text-align: left;
+    /deep/ .el-button {
+      padding: 10px;
+      margin-bottom: 10px;
+    }
+  }
+}
+</style>
