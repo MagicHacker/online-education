b4811f053bb91c64e2d4c8be29f784d5bd19a8ab
Johnson <zxmagichacker@gmail.com>
2020-03-11 14:49:08 +0800

    feat: 构建订单管理

--- src/views/order-manage/index.vue
+++ src/views/order-manage/index.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>22</div>
+  <div class="order-page-wrap">
+    <div class="order-search-wrap">
+      <el-input v-model="orderNumber" placeholder="订单编号"></el-input>
+      <el-input v-model="salesName" placeholder="买家姓名"></el-input>
+      <el-input v-model="courseStatus" placeholder="课程状态"></el-input>
+      <el-button type="primary">搜索</el-button>
+    </div>
+    <el-table border>
+      <el-table-column align="center" label="课程信息"></el-table-column>
+      <el-table-column align="center" label="课程老师"></el-table-column>
+      <el-table-column align="center" label="实付款"></el-table-column>
+      <el-table-column align="center" label="付款时间"></el-table-column>
+      <el-table-column align="center" label="买家"></el-table-column>
+      <el-table-column align="center" label="订单编号"></el-table-column>
+      <el-table-column align="center" label="交易状态"></el-table-column>
+    </el-table>
+    <pagination-view></pagination-view>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+import PaginationView from "../../components/pagination-view/index.vue";
+@Component({
+  components: {
+    PaginationView
+  }
+})
+export default class OrderManage extends Vue {
+  orderNumber = "";
+  salesName = "";
+  courseStatus = "";
+}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.order-page-wrap {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 30px 20px 0px;
+  .order-search-wrap {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    /deep/ .el-input {
+      width: 200px;
+    }
+  }
+}
+</style>
