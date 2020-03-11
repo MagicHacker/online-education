7b9083dfbfb325e695ceb2315d2082865170c2fd
Johnson <zxmagichacker@gmail.com>
2020-03-11 16:40:40 +0800

    feat: 构建课程管理

--- src/views/course-manage/index.vue
+++ src/views/course-manage/index.vue
@@ -1,8 +1,104 @@
 <template>
-  <div>22</div>
+  <div class="course-page-wrap">
+    <div class="course-search-wrap">
+      <el-input v-model="courseName" placeholder="课程名称"></el-input>
+      <el-input v-model="teacherName" placeholder="老师姓名"></el-input>
+      <el-input v-model="oneLevel" placeholder="一级分类"></el-input>
+      <el-input v-model="twoLevel" placeholder="二级分类"></el-input>
+      <el-date-picker
+        type="datetime"
+        v-model="createTime"
+        placeholder="创建时间"
+      ></el-date-picker>
+      <el-button type="primary">搜索</el-button>
+    </div>
+    <div class="course-table-wrap">
+      <el-button type="primary" size="mini" icon="el-icon-plus">新增</el-button>
+      <el-table border>
+        <el-table-column align="center" label="课程名称"></el-table-column>
+        <el-table-column align="center" label="一级分类"></el-table-column>
+        <el-table-column align="center" label="二级分类"></el-table-column>
+        <el-table-column align="center" label="预览图"></el-table-column>
+        <el-table-column align="center" label="Banner"></el-table-column>
+        <el-table-column align="center" label="老师"></el-table-column>
+        <el-table-column align="center" label="年级"></el-table-column>
+        <el-table-column align="center" label="价格"></el-table-column>
+        <el-table-column align="center" label="课时"></el-table-column>
+        <el-table-column align="center" label="展示状态"></el-table-column>
+        <el-table-column align="center" label="课程状态"></el-table-column>
+        <el-table-column align="center" label="课程目录"></el-table-column>
+        <el-table-column align="center" label="创建时间"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" @click="editItem(scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              size="mini"
+              type="success"
+              @click="changeStatus(scope.row)"
+              >上架</el-button
+            >
+            <el-button size="mini" type="danger" @click="deleteItem(scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <pagination-view></pagination-view>
+  </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({});
+import { Component, Vue } from "vue-property-decorator";
+import PaginationView from "../../components/pagination-view/index.vue";
+@Component({
+  components: {
+    PaginationView
+  }
+})
+export default class CourseManage extends Vue {
+  courseName = "";
+  teacherName = "";
+  oneLevel = "";
+  twoLevel = "";
+  createTime = "";
+  editItem(): void {
+    console.log("编辑");
+  }
+  changeStatus(): void {
+    console.log("上架");
+  }
+  deleteItem(): void {
+    console.log("删除");
+  }
+}
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.course-page-wrap {
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 30px 20px 0px;
+  .course-search-wrap {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    /deep/ .el-input {
+      width: 200px;
+    }
+  }
+  .course-table-wrap {
+    width: 100%;
+    margin-top: 20px;
+    text-align: left;
+    /deep/ .el-button {
+      padding: 10px;
+      margin-bottom: 10px;
+    }
+  }
+}
+</style>
