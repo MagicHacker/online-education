1d6fa410213a62ac774cec932c89ef73c6067d84
Johnson <zxmagichacker@gmail.com>
2020-03-27 16:16:31 +0800

    feat: 分页组件与父组件通信传参

--- src/components/pagination-view/index.vue
+++ src/components/pagination-view/index.vue
@@ -2,18 +2,27 @@
   <div class="pagination-view-wrap">
     <el-pagination
       :current-page="currentPage"
-      :page-sizes="[100, 200, 300]"
-      :page-size="100"
+      :page-sizes="[10, 20, 30]"
+      :page-size="10"
       layout="total, sizes,prev,pager,next,jumper"
-      :total="300"
+      :total="total"
+      @size-change="changePageSize"
+      @current-change="changeCurrent"
     ></el-pagination>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop } from 'vue-property-decorator'
 @Component
 export default class PaginationView extends Vue {
-  currentPage = 2;
+  currentPage = 1
+  @Prop() total
+  changePageSize(val: number): void {
+    this.$emit('changeSize', val)
+  }
+  changeCurrent(val: number): void {
+    this.$emit('changeCurrent', val)
+  }
 }
 </script>
 <style lang="less" scoped>
