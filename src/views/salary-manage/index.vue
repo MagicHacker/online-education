f3a93e5d25858eedd6dbad007941fc6370716133
Johnson <zxmagichacker@gmail.com>
2020-03-30 00:00:00 +0800

    feat: 收入管理绑定查询接口

--- src/views/salary-manage/index.vue
+++ src/views/salary-manage/index.vue
@@ -1,31 +1,19 @@
 <template>
   <div class="salary-page-wrap">
     <div class="salary-search-wrap">
-      <el-input
-        v-model="salaryName"
-        :clearable="true"
-        placeholder="教师姓名"
-      ></el-input>
-      <el-input
-        v-model="operateType"
-        :clearable="true"
-        placeholder="处理类型"
-      ></el-input>
-      <el-date-picker
-        v-model="dateTime"
-        type="datetime"
-        placeholder="申请时间"
-      ></el-date-picker>
+      <el-input v-model="teacherName" clearable placeholder="教师姓名"></el-input>
+      <el-input v-model="dealType" clearable placeholder="处理类型"></el-input>
+      <el-date-picker v-model="dateTime" type="date" placeholder="申请时间" clearable></el-date-picker>
       <el-button type="primary">搜索</el-button>
     </div>
-    <el-table border>
-      <el-table-column label="教师编号" align="center"></el-table-column>
-      <el-table-column label="教师姓名" align="center"></el-table-column>
-      <el-table-column label="处理类型" align="center"></el-table-column>
-      <el-table-column label="提现金额" align="center"></el-table-column>
-      <el-table-column label="申请时间" align="center"></el-table-column>
-      <el-table-column label="处理前金额" align="center"></el-table-column>
-      <el-table-column label="处理后金额" align="center"></el-table-column>
+    <el-table border :data="tableData">
+      <el-table-column label="教师编号" align="center" prop="teacher_id"></el-table-column>
+      <el-table-column label="教师姓名" align="center" prop="name"></el-table-column>
+      <el-table-column label="处理类型" align="center" prop="sal_type"></el-table-column>
+      <el-table-column label="提现金额" align="center" prop="sal_deal"></el-table-column>
+      <el-table-column label="申请时间" align="center" prop="deal_time"></el-table-column>
+      <el-table-column label="处理前金额" align="center" prop="sal_sum"></el-table-column>
+      <el-table-column label="处理后金额" align="center" prop="sal_after"></el-table-column>
     </el-table>
     <pagination-view></pagination-view>
   </div>
@@ -39,9 +27,29 @@ import PaginationView from "../../components/pagination-view/index.vue";
   }
 })
 export default class SalaryManage extends Vue {
-  salaryName = "";
-  operateType = "";
+  teacherName = "";
+  dealType = "";
   dateTime = "";
+  tableData = [];
+  total = 0;
+  mounted(): void {
+    this.queryData();
+  }
+  queryData(): void {
+    this.axios("http://localhost:3000/salary/getSalarys", {
+      params: {
+        teacherName: this.teacherName,
+        dealType: this.dealType,
+        dealTime: this.dateTime,
+        pageNum: 1,
+        pageSize: 10
+      }
+    }).then(res => {
+      const { result, total } = res.data;
+      this.tableData = result;
+      this.total = total;
+    });
+  }
 }
 </script>
 <style lang="less" scoped>
