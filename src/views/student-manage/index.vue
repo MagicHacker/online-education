d08a90a8b518c341aff26e29e5558bcc2aeda0fa
Johnson <zxmagichacker@gmail.com>
2020-03-30 18:30:24 +0800

    feat: 学生管理绑定查询接口，分页，模糊查询

--- src/views/student-manage/index.vue
+++ src/views/student-manage/index.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="student-page-wrap">
     <div class="student-search-wrap">
-      <el-input v-model="studentName" placeholder="姓名"></el-input>
-      <el-input v-model="phoneNumber" placeholder="手机号"></el-input>
+      <el-input v-model="studentName" placeholder="姓名" clearable></el-input>
+      <el-input v-model="phoneNumber" placeholder="手机号" clearable></el-input>
       <el-date-picker
         v-model="registerTime"
-        type="datetime"
+        type="date"
         placeholder="注册时间"
+        value-format="yyyy-MM-dd"
+        clearable
       ></el-date-picker>
-      <el-button type="primary">搜索</el-button>
+      <el-button type="primary" @click="search">搜索</el-button>
     </div>
-    <el-table border>
-      <el-table-column label="姓名"></el-table-column>
-      <el-table-column label="手机号"></el-table-column>
-      <el-table-column label="已购课程"></el-table-column>
-      <el-table-column label="注册时间"></el-table-column>
+    <el-table border :data="tableData">
+      <el-table-column label="姓名" prop="name"></el-table-column>
+      <el-table-column label="手机号" prop="phone"></el-table-column>
+      <el-table-column label="已购课程" prop="count"></el-table-column>
+      <el-table-column label="注册时间" prop="rgtTime"></el-table-column>
     </el-table>
-    <pagination-view></pagination-view>
+    <pagination-view
+      :total="total"
+      @changeSize="changePageSize"
+      @changeCurrent="changeCurrentPage"
+    ></pagination-view>
   </div>
 </template>
 <script lang="ts">
@@ -31,6 +37,41 @@ export default class StudentManage extends Vue {
   studentName = "";
   phoneNumber = "";
   registerTime = "";
+  pageNum = 1;
+  pageSize = 10;
+  tableData = [];
+  total = 0;
+  mounted(): void {
+    this.queryData();
+  }
+  queryData(): void {
+    this.axios("http://localhost:3000/student/getStudents", {
+      params: {
+        name: this.studentName,
+        phone: this.phoneNumber,
+        rgtTime: this.registerTime,
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }
+    }).then(res => {
+      const { code, result, total } = res.data;
+      if (code === 0) {
+        this.tableData = result;
+        this.total = total;
+      }
+    });
+  }
+  search(): void {
+    this.queryData();
+  }
+  changePageSize(val: number): void {
+    this.pageSize = val;
+    this.queryData();
+  }
+  changeCurrentPage(val: number): void {
+    this.pageNum = val;
+    this.queryData();
+  }
 }
 </script>
 <style lang="less" scoped>
