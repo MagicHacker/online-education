6a55de682d66a1174c28aa4984840994ad3e1d81
Johnson <zxmagichacker@gmail.com>
2020-04-01 10:46:45 +0800

    feat: 订单管理绑定查询接口

--- src/views/order-manage/index.vue
+++ src/views/order-manage/index.vue
@@ -1,21 +1,61 @@
 <template>
   <div class="order-page-wrap">
     <div class="order-search-wrap">
-      <el-input v-model="orderNumber" placeholder="订单编号"></el-input>
-      <el-input v-model="salesName" placeholder="买家姓名"></el-input>
-      <el-input v-model="courseStatus" placeholder="课程状态"></el-input>
-      <el-button type="primary">搜索</el-button>
+      <el-input
+        v-model="orderNumber"
+        placeholder="订单编号"
+        clearable
+      ></el-input>
+      <el-input v-model="salesName" placeholder="买家姓名" clearable></el-input>
+      <el-input
+        v-model="courseStatus"
+        placeholder="课程状态"
+        clearable
+      ></el-input>
+      <el-button type="primary" @click="search">搜索</el-button>
     </div>
-    <el-table border>
-      <el-table-column align="center" label="课程信息"></el-table-column>
-      <el-table-column align="center" label="课程老师"></el-table-column>
-      <el-table-column align="center" label="实付款"></el-table-column>
-      <el-table-column align="center" label="付款时间"></el-table-column>
-      <el-table-column align="center" label="买家"></el-table-column>
-      <el-table-column align="center" label="订单编号"></el-table-column>
-      <el-table-column align="center" label="交易状态"></el-table-column>
+    <el-table border :data="tableData">
+      <el-table-column
+        align="center"
+        label="课程信息"
+        prop="courseName"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="课程老师"
+        prop="teacherName"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="实付款"
+        prop="pay_amount"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="付款时间"
+        prop="pay_time"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="买家"
+        prop="studentName"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="订单编号"
+        prop="order_id"
+      ></el-table-column>
+      <el-table-column
+        align="center"
+        label="交易状态"
+        prop="courseStatus"
+      ></el-table-column>
     </el-table>
-    <pagination-view></pagination-view>
+    <pagination-view
+      :total="total"
+      @changeSize="changePageSize"
+      @changeCurrent="changeCurrentPage"
+    ></pagination-view>
   </div>
 </template>
 <script lang="ts">
@@ -30,6 +70,41 @@ export default class OrderManage extends Vue {
   orderNumber = "";
   salesName = "";
   courseStatus = "";
+  tableData = [];
+  total = 0;
+  pageNum = 1;
+  pageSize = 10;
+  mounted(): void {
+    this.queryData();
+  }
+  queryData(): void {
+    this.axios("http://localhost:3000/order/getOrders", {
+      params: {
+        orderId: this.orderNumber,
+        buyerName: this.salesName,
+        courseStatus: this.courseStatus,
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }
+    }).then(res => {
+      const { code, result, total } = res.data;
+      if (code === 0) {
+        this.tableData = result;
+        this.total = total;
+      }
+    });
+  }
+  search(): void {
+    this.queryData();
+  }
+  changePageSize(val: number): void {
+    this.pageSize = val;
+    this.queryData();
+  }
+  changeCurrentPage(val: number): void {
+    this.pageNum = val;
+    this.queryData();
+  }
 }
 </script>
 <style lang="less" scoped>
