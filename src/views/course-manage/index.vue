eec065f838008945fbf5d59a0857f14c88f61385
Johnson <zxmagichacker@gmail.com>
2020-04-11 21:04:21 +0800

    feat: 添加编辑和上下架功能

--- src/views/course-manage/index.vue
+++ src/views/course-manage/index.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="course-page-wrap">
     <div class="course-search-wrap">
-      <el-input
-        v-model="courseName"
-        placeholder="课程名称"
-        clearable
-      ></el-input>
-      <el-input
-        v-model="teacherName"
-        placeholder="教师姓名"
-        clearable
-      ></el-input>
+      <el-input v-model="courseName" placeholder="课程名称" clearable></el-input>
+      <el-input v-model="teacherName" placeholder="教师姓名" clearable></el-input>
       <el-input v-model="oneLevel" placeholder="一级分类" clearable></el-input>
       <el-input v-model="twoLevel" placeholder="二级分类" clearable></el-input>
       <el-date-picker
@@ -23,106 +15,51 @@
       <el-button type="primary" @click="search">搜索</el-button>
     </div>
     <div class="course-table-wrap">
-      <el-button type="primary" size="mini" icon="el-icon-plus" @click="addItem"
-        >新增</el-button
-      >
+      <el-button type="primary" size="mini" icon="el-icon-plus" @click="addItem">新增</el-button>
       <el-table border :data="tableData">
-        <el-table-column
-          align="center"
-          label="课程名称"
-          prop="name"
-          width="120px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="一级分类"
-          prop="one_level"
-          width="50px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="二级分类"
-          prop="two_level"
-          width="50px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="预览图"
-          width="80px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="Banner"
-          width="80px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="教师姓名"
-          prop="teacherName"
-          width="80px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="年级"
-          prop="grade"
-          width="50px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="价格"
-          prop="price"
-          width="80px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="课时单位"
-          prop="course_unit"
-          width="50px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="展示状态"
-          prop="show_status"
-          width="50px"
-        ></el-table-column>
-        <el-table-column
-          align="center"
-          label="课程状态"
-          prop="course_status"
-          width="80px"
-        ></el-table-column>
-        <el-table-column align="center" label="课程目录" width="80px">
-          <template><span>查看详情</span></template>
+        <el-table-column align="center" label="课程名称" prop="name" width="120px"></el-table-column>
+        <el-table-column align="center" label="一级分类" prop="one_level" width="50px"></el-table-column>
+        <el-table-column align="center" label="二级分类" prop="two_level" width="50px"></el-table-column>
+        <el-table-column align="center" label="预览图" width="80px"></el-table-column>
+        <el-table-column align="center" label="Banner" width="80px"></el-table-column>
+        <el-table-column align="center" label="教师姓名" prop="teacherName" width="80px"></el-table-column>
+        <el-table-column align="center" label="年级" prop="grade" width="50px"></el-table-column>
+        <el-table-column align="center" label="价格" prop="price" width="80px"></el-table-column>
+        <el-table-column align="center" label="课时单位" prop="course_unit" width="50px"></el-table-column>
+        <el-table-column align="center" label="展示状态" width="50px">
+          <template slot-scope="scope">{{ scope.row.show_status === 0 ? "下架" : "上架" }}</template>
         </el-table-column>
-        <el-table-column
-          align="center"
-          label="创建时间"
-          prop="createTime"
-          width="100px"
-        ></el-table-column>
+        <el-table-column align="center" label="课程状态" prop="course_status" width="80px">
+          <template slot-scope="scope">
+            {{
+            scope.row.course_status === 0
+            ? "未开课"
+            : scope.row.course_status === 1
+            ? "开课中"
+            : "已结课"
+            }}
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="课程目录" width="80px">
+          <template>
+            <span>查看详情</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="创建时间" prop="createTime" width="100px"></el-table-column>
         <el-table-column align="center" label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" @click="editItem(scope.row)"
-              >编辑</el-button
-            >
+            <el-button size="mini" type="primary" @click="editItem(scope.row)">编辑</el-button>
             <el-button
               size="mini"
               type="success"
               @click="changeStatus(scope.row)"
-              >上架</el-button
-            >
-            <el-button size="mini" type="danger" @click="deleteItem(scope.row)"
-              >删除</el-button
-            >
+            >{{ scope.row.show_status === 0 ? "上架" : "下架" }}</el-button>
+            <el-button size="mini" type="danger" @click="deleteItem(scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
-    <pagination-view
-      :total="total"
-      @changeSize="changePageSize"
-      @changeCurrent="changeCurrentPage"
-    ></pagination-view>
+    <pagination-view :total="total" @changeSize="changePageSize" @changeCurrent="changeCurrentPage"></pagination-view>
   </div>
 </template>
 <script lang="ts">
@@ -178,11 +115,22 @@ export default class CourseManage extends Vue {
     this.pageNum = val;
     this.queryData();
   }
-  editItem(): void {
-    console.log("编辑");
+  editItem(row): void {
+    const courseId = row.course_id;
+    this.$router.push({ name: "newCourse", params: { courseId } });
   }
-  changeStatus(): void {
-    console.log("上架");
+  changeStatus(row): void {
+    this.axios
+      .post("http://localhost:3000/course/updateCourse", {
+        courseId: row.course_id,
+        showStatus: row.show_status === 0 ? 1 : 0
+      })
+      .then(res => {
+        const { code } = res.data;
+        if (code === 0) {
+          this.queryData();
+        }
+      });
   }
   deleteItem(): void {
     console.log("删除");
