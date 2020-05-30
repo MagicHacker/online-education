1d693c9e53c323e86f71178e75e4d8b668f1e0c7
Johnson <zxmagichacker@gmail.com>
2020-05-30 16:21:55 +0800

    feat: 添加DevServer配置

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,40 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
-const resolve = dir => path.join(__dirname, dir);
-const debug = process.env.Node_ENV !== "production";
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, dir)
+const debug = process.env.Node_ENV !== 'production'
 module.exports = {
-  configureWebpack: config => {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    hotOnly: true
+  },
+  configureWebpack: (config) => {
     // 配置sourcemap
     if (debug) {
-      config.devtool = "eval-source-map";
+      config.devtool = 'eval-source-map'
     }
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
-      .rule("svg")
-      .exclude.add(resolve("./src/icons"))
-      .end();
-    config.module
-      .rule("icon")
-      .test(/\.svg$/)
-      .include.add(resolve("./src/icons"))
+      .rule('svg')
+      .exclude.add(resolve('./src/icons'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+    config.module
+      .rule('icon')
+      .test(/\.svg$/)
+      .include.add(resolve('./src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]"
+        symbolId: 'icon-[name]'
       })
       .end()
-      .use("svgo-loader")
-      .loader("svgo-loader")
+      .use('svgo-loader')
+      .loader('svgo-loader')
       .options({
-        externalConfig: "svgo-config.yml"
-      });
+        externalConfig: 'svgo-config.yml'
+      })
   }
-};
+}
